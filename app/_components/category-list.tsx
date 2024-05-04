262831e3bef9d66371fb204502d5476d16e4ca2f
Enrico <enricosecco@edu.unifil.br>
2024-05-04 16:28:06 -0300

    feat: add category items and list components

---
+++ app/_components/category-list.tsx
@@ -0,0 +1,17 @@
+//Server component, capaz de conectar com o banco.
+import { db } from "../_lib/prisma";
+import CategoryItem from "./category-item";
+
+const CategoryList = async () => {
+  const categories = await db.category.findMany({});
+
+  return (
+    <div className="grid grid-cols-2 gap-3">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default CategoryList;
