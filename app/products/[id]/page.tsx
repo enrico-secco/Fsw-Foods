af785b43418b69ecabecb5c681429e878ede0103
Enrico <enricosecco@edu.unifil.br>
2024-05-11 17:40:00 -0300

    feat: add product page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,47 @@
+import { db } from "@/app/_lib/prisma";
+import { notFound } from "next/navigation";
+import ProductImage from "./components/product-image";
+import ProductDetails from "./components/product-details";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const ProductPage = async ({ params: { id } }: ProductPageProps) => {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      {/* Separado em outro componente pois vai ser um client devido ao router */}
+      <ProductImage product={product} />
+
+      <ProductDetails product={product} complementaryProducts={juices} />
+    </div>
+  );
+};
+
+export default ProductPage;
