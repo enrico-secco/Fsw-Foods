4ca1a2f98dd636557850dd883c00db71ca82adcc
Enrico <enricosecco@edu.unifil.br>
2024-05-24 14:00:19 -0300

    feat: add cart and cart item components

---
+++ app/_context/cart.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import { Prisma } from "@prisma/client";
+import { ReactNode, createContext, useMemo, useState } from "react";
+import { calculateProductTotalPrice } from "../_helpers/price";
+
+export interface CartProduct
+  extends Prisma.ProductGetPayload<{
+    include: { restaurant: { select: { deliveryFee: true } } };
+  }> {
+  quantity: number;
+}
+
+interface ICardContext {
+  products: CartProduct[];
+  subtotalPrice: number;
+  totalPrice: number;
+  totalDiscounts: number;
+  // eslint-disable-next-line no-unused-vars
+  addProductToCart: (
+    // eslint-disable-next-line no-unused-vars
+    product: Prisma.ProductGetPayload<{
+      include: { restaurant: { select: { deliveryFee: true } } };
+    }>,
+    // eslint-disable-next-line no-unused-vars
+    quantity: number,
+  ) => void;
+  // eslint-disable-next-line no-unused-vars
+  decreaseProductQuantity: (productId: string) => void;
+  // eslint-disable-next-line no-unused-vars
+  increaseProductQuantity: (productId: string) => void;
+  // eslint-disable-next-line no-unused-vars
+  removeProductFromCart: (productId: string) => void;
+}
+
+export const CartContext = createContext<ICardContext>({
+  products: [],
+  subtotalPrice: 0,
+  totalDiscounts: 0,
+  totalPrice: 0,
+  addProductToCart: () => {},
+  decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
+  removeProductFromCart: () => {},
+});
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const subtotalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + Number(product.price) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + calculateProductTotalPrice(product) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalDiscounts = subtotalPrice - totalPrice;
+
+  const decreaseProductQuantity = (productId: string) => {
+    return setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          if (cartProduct.quantity === 1) {
+            return cartProduct;
+          }
+
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity - 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const increaseProductQuantity = (productId: string) => {
+    return setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const removeProductFromCart = (productId: string) => {
+    return setProducts((prev) =>
+      prev.filter((cartProduct) => cartProduct.id !== productId),
+    );
+  };
+
+  const addProductToCart = (
+    product: Prisma.ProductGetPayload<{
+      include: { restaurant: { select: { deliveryFee: true } } };
+    }>,
+    quantity: number,
+  ) => {
+    const isProductAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+
+    if (isProductAlreadyOnCart) {
+      return setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + quantity,
+            };
+          }
+
+          return cartProduct;
+        }),
+      );
+    }
+
+    setProducts((prev) => [...prev, { ...product, quantity: quantity }]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        addProductToCart,
+        decreaseProductQuantity,
+        increaseProductQuantity,
+        removeProductFromCart,
+        subtotalPrice,
+        totalDiscounts,
+        totalPrice,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
